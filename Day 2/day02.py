2e17a77ffa203efae22c226467d6ca955c709823
LAZ-R <crazydrawer62@gmail.com>
2020-12-05 18:50:08 +0100

    First attempt for Day 02

---
+++ Day 2/day02.py
@@ -0,0 +1,22 @@
+with open(r"input.txt", "r") as f:
+    validPasswords=0
+    fullList=(f.read().split())
+    i=0
+    while i < len(fullList):
+        a = fullList[i].split("-")
+        low = int(a[0]) # lowest iteration number of the searched letter you have to have to get your password validated
+        high = int(a[1]) # highest iteration number of the searched letter you have to have to get your password validated
+        b = fullList[i+1].split(":")
+        searchedLetter = b[0] # searched letter
+        password = fullList[i+2] # actual password
+        rankInStr=0
+        while rankInStr<len(password):
+            iteration=0
+            individualCharacter=password[rankInStr]
+            if  individualCharacter == searchedLetter:
+                iteration+=1
+            rankInStr+=1
+        if low <= iteration <= high:
+            validPasswords+=1
+        i+=3
+    print("\n Number of valid passwords :", validPasswords)       
