f147821c14e2ec9e38cc05057c992f0a3db9365f
LAZ-R <crazydrawer62@gmail.com>
2020-12-07 18:40:49 +0100

    tried to compact day 02, failed miserably

---
+++ Day 2 V1/day02.py
@@ -0,0 +1,61 @@
+def day02part01():
+    with open(r"Day 2\input.txt", "r") as f:
+        validPasswords = 0
+        fullList = (f.read().split())
+        i = 0
+        while i < len(fullList):
+            a = fullList[i].split("-")
+            # lowest iteration of the searched letter you have to have to get your password validated
+            low = int(a[0])
+            # highest iteration of the searched letter you have to have to get your password validated
+            high = int(a[1])
+            b = fullList[i+1].split(":")
+            searchedLetter = b[0]  # searched letter
+            password = fullList[i+2]  # actual password
+            rankInStr = 0
+            iteration = 0
+            while rankInStr < len(password):
+                individualCharacter = password[rankInStr]
+                if individualCharacter == searchedLetter:
+                    iteration += 1
+                rankInStr += 1
+            if low <= iteration <= high:
+                validPasswords += 1
+            i += 3
+        print("\nNumber of valid passwords :", validPasswords)
+
+
+def day02part02():
+    with open(r"Day 2\input.txt", "r") as f:
+        validPasswords = 0
+        fullList = (f.read().split())
+        i = 0
+        while i < len(fullList):
+            a = fullList[i].split("-")
+            # lowest iteration of the searched letter you have to have to get your password validated
+            positionOne = int(a[0])
+            # highest iteration of the searched letter you have to have to get your password validated
+            positionTwo = int(a[1])
+            b = fullList[i+1].split(":")
+            searchedLetter = b[0]  # searched letter
+            password = fullList[i+2]  # actual password
+            rankInStr = 0
+            iteration = 0
+            while rankInStr < len(password):
+                individualCharacter = password[rankInStr]
+                if individualCharacter == searchedLetter:
+                    if rankInStr == positionOne-1:
+                        iteration += 1
+                    if rankInStr == positionTwo-1:
+                        iteration += 1
+                rankInStr += 1
+            if iteration == 1:
+                validPasswords += 1
+            i += 3
+        print("\nNumber of valid passwords :", validPasswords)
+
+
+print("\n\tDAY 02 - Part 01")
+day02part01()
+print("\n\tDAY 02 - Part 02")
+day02part02()
--- Day 2/day02.py
+++ Day 2/day02.py
@@ -1,61 +1,63 @@
+def passwordCheck01(min, max,  letter, pswrd):
+    iteration = 0
+    a = 0
+    while a < len(pswrd):
+        if pswrd[a] == letter:
+            iteration += 1
+        a += 1
+    if iteration in range(min, max+1):
+        return True
+
+
 def day02part01():
-    with open(r"Day 2\input.txt", "r") as f:
-        validPasswords = 0
-        fullList = (f.read().split())
-        i = 0
-        while i < len(fullList):
-            a = fullList[i].split("-")
-            # lowest iteration of the searched letter you have to have to get your password validated
-            low = int(a[0])
-            # highest iteration of the searched letter you have to have to get your password validated
-            high = int(a[1])
-            b = fullList[i+1].split(":")
-            searchedLetter = b[0]  # searched letter
-            password = fullList[i+2]  # actual password
-            rankInStr = 0
-            iteration = 0
-            while rankInStr < len(password):
-                individualCharacter = password[rankInStr]
-                if individualCharacter == searchedLetter:
-                    iteration += 1
-                rankInStr += 1
-            if low <= iteration <= high:
-                validPasswords += 1
-            i += 3
-        print("\nNumber of valid passwords :", validPasswords)
+    with open("day 2\input.txt", "r") as f:
+        lineTotal = len(f.read().split("\n"))
+    with open("day 2\input.txt", "r") as f:
+        okPassword = 0
+        lineNmbr = 0
+        while lineNmbr < lineTotal:
+            line = (f.readline().split())
+            iterRange = line[0].split("-")
+            iterMin = int(iterRange[0])
+            iterMax = int(iterRange[1])
+            letterToSearch = line[1].split(":")[0]
+            password = line[2]
+            if passwordCheck01(iterMin, iterMax, letterToSearch, password):
+                okPassword += 1
+            lineNmbr += 1
+        print(okPassword)
+
+
+def passwordCheck02(min, max,  letter, pswrd):
+    iteration = 0
+    a = 0
+    while a < len(pswrd):
+        if pswrd[a] == letter:
+            if a+1 == min or a+1 == max:
+                iteration += 1
+        a += 1
+    if iteration == 1:
+        return True
 
 
 def day02part02():
-    with open(r"Day 2\input.txt", "r") as f:
-        validPasswords = 0
-        fullList = (f.read().split())
-        i = 0
-        while i < len(fullList):
-            a = fullList[i].split("-")
-            # lowest iteration of the searched letter you have to have to get your password validated
-            positionOne = int(a[0])
-            # highest iteration of the searched letter you have to have to get your password validated
-            positionTwo = int(a[1])
-            b = fullList[i+1].split(":")
-            searchedLetter = b[0]  # searched letter
-            password = fullList[i+2]  # actual password
-            rankInStr = 0
-            iteration = 0
-            while rankInStr < len(password):
-                individualCharacter = password[rankInStr]
-                if individualCharacter == searchedLetter:
-                    if rankInStr == positionOne-1:
-                        iteration += 1
-                    if rankInStr == positionTwo-1:
-                        iteration += 1
-                rankInStr += 1
-            if iteration == 1:
-                validPasswords += 1
-            i += 3
-        print("\nNumber of valid passwords :", validPasswords)
+    with open("day 2\input.txt", "r") as f:
+        lineTotal = len(f.read().split("\n"))
+    with open("day 2\input.txt", "r") as f:
+        okPassword = 0
+        lineNmbr = 0
+        while lineNmbr < lineTotal:
+            line = (f.readline().split())
+            iterRange = line[0].split("-")
+            iterMin = int(iterRange[0])
+            iterMax = int(iterRange[1])
+            letterToSearch = line[1].split(":")[0]
+            password = line[2]
+            if passwordCheck02(iterMin, iterMax, letterToSearch, password):
+                okPassword += 1
+            lineNmbr += 1
+        print(okPassword)
 
 
-print("\n\tDAY 02 - Part 01")
 day02part01()
-print("\n\tDAY 02 - Part 02")
 day02part02()
